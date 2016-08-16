5de44171573901b47a16c13f527ea0a918890944
petrosliu <petros.ydliu@gmail.com>
2016-08-16 06:30:12 -0700

    first pokedex demo

--- public/core.js
+++ public/core.js
@@ -1,35 +1,44 @@
-var petrosTodo = angular.module('petrosTodo', []);
+var pokememo = angular.module('pokememo', []);
+
 function mainController($scope, $http) {
     $scope.formData = {};
     // when landing on the page, get all todos and show them
     $http.get('/api/todos')
-        .success(function (data) {
+        .success(function(data) {
             $scope.todos = data;
             console.log('23' + data);
         })
-        .error(function (data) {
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    $http.get('/api/pokemons')
+        .success(function(data) {
+            $scope.pokemons = data;
+        })
+        .error(function(data) {
             console.log('Error: ' + data);
         });
     // when submitting the add form, send the text to the node API
-    $scope.createTodo = function () {
+    $scope.createTodo = function() {
         $http.post('/api/todos', $scope.formData)
-            .success(function (data) {
+            .success(function(data) {
                 $scope.formData = {};
                 $scope.todos = data;
                 console.log(data);
             })
-            .error(function (data) {
+            .error(function(data) {
                 console.log('Error: ' + data);
             });
     };
     // delete a todo after checking it
-    $scope.deleteTodo = function (id) {
+    $scope.deleteTodo = function(id) {
         $http.delete('/api/todos/' + id)
-            .success(function (data) {
+            .success(function(data) {
                 $scope.todos = data;
                 console.log(data);
             })
-            .error(function (data) {
+            .error(function(data) {
                 console.log('Error: ' + data);
             });
     };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express  = require('express');
-var app      = express();
+var express = require('express');
+var app = express();
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://admin:admin@ds040489.mlab.com:40489/pokemon');
 app.configure(function() {
     // set the static files location /public/img will be /img for users
@@ -16,8 +17,21 @@ console.log("App listening on port 8080");
 
 // ----- define model
 var Todo = mongoose.model('Todo', {
-    text : String
+    text: String
 });
+
+var pokemon = require('./models/pokemon');
+var PokemonDB = mongoose.model('Pokemon', pokemon.schema);
+PokemonDB.collection.remove({});
+PokemonDB.collection.insert(pokemon.allPokemons, function(err) {
+    if (err) {
+        console.log('Oh?');
+    } else {
+        console.log('%d pokemons were successfully loaded in Pokedex.', pokemon.allPokemons.length);
+    }
+});
+
+
 
 // ----- define routes
 // get all todos
@@ -31,12 +45,24 @@ app.get('/api/todos', function(req, res) {
         res.json(todos); // return all todos in JSON format
     });
 });
+
+app.get('/api/pokemons', function(req, res) {
+    // use mongoose to get all todos in the database
+    PokemonDB.find(function(err, pokemons) {
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err) {
+            res.send(err);
+        }
+        res.json(pokemons); // return all todos in JSON format
+    });
+});
+
 // create Todo and send back all todos after creation
 app.post('/api/todos', function(req, res) {
     // create a Todo, information comes from AJAX request from Angular
     Todo.create({
-        text : req.body.text,
-        done : false
+        text: req.body.text,
+        done: false
     }, function(err, todo) {
         if (err) {
             res.send(err);
@@ -53,7 +79,7 @@ app.post('/api/todos', function(req, res) {
 // delete a todo
 app.delete('/api/todos/:todo_id', function(req, res) {
     Todo.remove({
-        _id : req.params.todo_id
+        _id: req.params.todo_id
     }, function(err, todo) {
         if (err)
             res.send(err);
